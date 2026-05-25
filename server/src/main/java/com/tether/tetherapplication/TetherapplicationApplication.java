d92fe07a8fa2c32e34bf6ed3f92c6fdbe2ba562c
NavPilDev <abhinavpillai04@gmail.com>
2026-05-25 13:17:47 -0500

    next+spring setup

---
+++ server/src/main/java/com/tether/tetherapplication/TetherapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.tether.tetherapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetherapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetherapplicationApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/tether/tetherapplication/TetherapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tether.tetherapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetherapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
